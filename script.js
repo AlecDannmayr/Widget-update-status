43e14b0a80d66651663a6b0d24ededd7f8594bcb
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-04-09 12:30:42 +0100

    --- Auto-Git Commit ---

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
 window.addEventListener("load", function () {
-  var loadingScreen = document.getElementById("loading-screen");
-  var bodyWrapper = document.getElementById("body-wrapper");
-  loadingScreen.style.display = "none";
-  bodyWrapper.style.display = "block";
-});
-
-$(document).ready(function () {
-  function toggleSidebar() {
-    $(".button").toggleClass("active");
-    $("main").toggleClass("move-to-left");
-    $(".sidebar-item").toggleClass("active");
-  }
-
-  $(".button").on("click tap", function () {
-    toggleSidebar();
-  });
-
-  $(document).keyup(function (e) {
-    if (e.keyCode === 27) {
-      toggleSidebar();
+  var e = document.getElementById("loading-screen"),
+    t = document.getElementById("body-wrapper");
+  (e.style.display = "none"), (t.style.display = "block");
+}),
+  $(document).ready(function () {
+    function e() {
+      $(".button").toggleClass("active"),
+        $("main").toggleClass("move-to-left"),
+        $(".sidebar-item").toggleClass("active");
     }
+    $(".button").on("click tap", function () {
+      e();
+    }),
+      $(document).keyup(function (t) {
+        27 === t.keyCode && e();
+      });
   });
-});
