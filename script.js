69d9718a1ec62977eba0b11d06b5f8cd1de0717c
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-04-09 11:30:24 +0100

    --- Auto-Git Commit ---

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-fetch("http://feed.decimalcricket.com/api/match/list", {
-  method: "GET",
-  headers: {
-    "x-api-key": "decimaltest",
-    "Content-Type": "application/json",
-  },
+$(document).ready(function () {
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function () {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
 });
-
-
-   $(document).ready(function () {
-     function toggleSidebar() {
-       $(".button").toggleClass("active");
-       $("main").toggleClass("move-to-left");
-       $(".sidebar-item").toggleClass("active");
-     }
-
-     $(".button").on("click tap", function () {
-       toggleSidebar();
-     });
-
-     $(document).keyup(function (e) {
-       if (e.keyCode === 27) {
-         toggleSidebar();
-       }
-     });
-   });
